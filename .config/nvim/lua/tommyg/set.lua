ce5c2edadd4f77e2879204cc6db5add83ff1cad3
TomasGadea <tomas@dribia.com>
2024-06-13 13:02:40 +0200

    limit col --> 88

--- .config/nvim/lua/tommyg/set.lua
+++ .config/nvim/lua/tommyg/set.lua
@@ -13,7 +13,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "88"
 vim.opt.guicursor = ""
 vim.opt.splitright = true
 vim.opt.cursorline = true
